fc3ce897d01f58340d76f25bde17317ac805025c
Alayna Johnston <55775759+ayyjayy2@users.noreply.github.com>
2023-06-29 11:45:57 -0400

    checkride data files created for flashcards

---
+++ src/app/flashcards/components/checkride-flashcards/assets/checkride-data.ts
@@ -0,0 +1,15 @@
+import { Flashcards } from "../../../flashcards.model";
+
+export const CHECKRIDEDATA: Flashcards[] = [
+    { question: 'What documents must you have with you to fly the airplane? (61.3)', 
+        answer: 'Pilot certificate, medical certificate, and government issued photo ID. *as a student pilot, these documents in addition to your logbook (to show endorsements) are only required when acting as PIC (solo flight)' },
+    { question: 'What are your limitations as a student pilot? (61.89)', 
+        answer: '1. No passengers. 2. Cannot carry property for compensation or hire. 3. Cannot fly in furtherance of a business. 4. Cannot fly with a flight or surface visibility of less than 3sm during the day and 5sm at night and flight must be made with visual references to the surface.'},
+    { question: 'What privileges and limitations apply to private pilot? (61.113) (91.146)', 
+        answer: 'Privileges – Act as PIC and carry passengers – conduct search and rescue operations; fly for charitable, non-profit, or community event; act as an aircraft salesman if you have at least 200 hours. Limitations – Cannot fly for hire; must pay no less than pro rata share' }
+];
+
+// export const CHECKRIDEDATA = [
+//     new Flashcard('What documents must you have with you to fly the airplane? (61.3)','Pilot certificate, medical certificate, and government issued photo ID. *as a student pilot, these documents in addition to your logbook (to show endorsements) are only required when acting as PIC (solo flight)'),
+//     new Flashcard('What are your limitations as a student pilot? (61.89)', '1. No passengers. 2. Cannot carry property for compensation or hire. 3. Cannot fly in furtherance of a business. 4. Cannot fly with a flight or surface visibility of less than 3sm during the day and 5sm at night and flight must be made with visual references to the surface.')
+// ]
